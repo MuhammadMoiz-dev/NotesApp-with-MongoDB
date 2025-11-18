4f62a57752f6bb008ff8df513cc398c83b4bc177
MuhammadMoiz-dev <mmoiz2162@gmail.com>
2025-11-18 20:43:12 +0500

    update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:5173", // your frontend URL
+        origin: "https://notes-app-with-mongo-db-ccnn.vercel.app/", // your frontend URL
         credentials: true,               // allow sending cookies
     })
 );
